d04cc4e1e3db727e7282c037c538ba64edf5f1c3
Christiaan Steenkist <chnstone13backup@gmail.com>
2017-02-08 15:48:20 +0100

    I did all I could

--- fixed report/src/a14/binopsbase.h
+++ fixed report/src/a14/binopsbase.h
@@ -1,30 +1,13 @@
 #ifndef BINOPSBASE_H
 #define BINOPSBASE_H
 
-#include "operations.h"
-
-template <typename Binops, 
-    typename Derived, int ...operators>
-class BinopsBase0
-{};
-
-template <typename Binops, typename Derived>
-class BinopsBase0<Binops, Derived, 0>
-{};
-
 template <typename Derived, int ...operators>
-class BinopsBase : public BinopsBase0<BinopsBase<
-    Derived, operators...>, Derived, operators...>
+class BinopsBase
 {
 	friend Derived &operator+(Derived &lhs, 
 	    Derived const &rhs);
 	friend Derived &operator*(Derived &lhs, 
 	    Derived const &rhs);
-	
-	friend Add<BinopsBase<Derived, operators...>, 
-	    Derived>;
-	friend Mul<BinopsBase<Derived, operators...>, 
-	    Derived>;
 	
 	void addWrap(Derived const &rhs)
 	{
--- fixed report/src/a9/type.h
+++ fixed report/src/a9/type.h
@@ -9,9 +9,13 @@ using namespace std;
 // Generic case: no Needle found
 template <typename Needle, size_t index, 
     typename Other, typename ...Stack>
-class TypeIdx : public TypeIdx<Needle, 
-    index + 1, Stack...>
-{};
+class TypeIdx
+{
+	public:
+		enum { result = TypeIdx<Needle, index + 1,
+			Stack...>::located };
+		enum { located = (result == 0 ? 0 : result)};
+};
 
 // Exit case: Needle found
 template <typename Needle, size_t index, 
@@ -20,14 +24,6 @@ class TypeIdx<Needle, index, Needle, Stack...>
 {
 	public:
 		enum { located = index + 1 };
-};
-
-// Exit case: Needle found
-template <typename Needle, size_t index>
-class TypeIdx<Needle, index, Needle>
-{
-	public:
-		enum { located = index + 1};
 };
 
 // Exit case: No more hay
@@ -39,18 +35,33 @@ class TypeIdx<Needle, index, Other>
 		enum { located = 0 };
 };
 
+// Exit case: No more hay + Needle found
+// Disambiguates between "Needle found" and
+//  "no more hay"
+template <typename Needle, size_t index>
+class TypeIdx<Needle, index, Needle>
+{
+	public:
+		enum { located = index + 1 };
+};
+
 // TYPE STARTER
 // Starter for TypeIdx and looping
 template <typename Needle, typename ...Stack>
-class Type : public TypeIdx<Needle, 0, Stack...>
-{};
+class Type
+{
+	public:
+		enum { located =
+			TypeIdx<Needle, 0, Stack...>::located };
+};
 
-// Starter for empty haystack
+// Empty stack specialization because TypeIdx
+//  requires at least one item in the stack
 template <typename Needle>
 class Type<Needle>
 {
 	public:
-		enum { located = 0};
+		enum { located = 0 };
 };
 
 #endif
